a6d2853c989fd55358e19bcb5a91d70831e2329e
rito528 <39003544+rito528@users.noreply.github.com>
2025-05-12 01:25:52 +0900

    feat: ItemStackPersistence を gachadata に向けて実装

---
+++ src/common/scala/click/seichi/infra/JdbcGachaDataItemStackPersistence.scala
@@ -0,0 +1,58 @@
+package click.seichi.infra
+
+import cats.data.NonEmptyVector
+import cats.effect.Sync
+import click.seichi.domain.DeserializedItemStacksWithPath
+import click.seichi.domain.persistence.ItemStackPersistence
+import click.seichi.domain.Path
+import click.seichi.domain.Segment
+import click.seichi.typeclasses.SerializeAndDeserialize
+import scalikejdbc._
+import click.seichi.domain.DeserializedItemStacks
+
+class JdbcGachaDataItemStackPersistence[F[_]: Sync, ItemStack](
+  implicit serializeAndDeserialize: SerializeAndDeserialize[Nothing, ItemStack]
+) extends ItemStackPersistence[F, ItemStack] {
+  override def readSerializedItemStacks: F[Vector[DeserializedItemStacksWithPath[ItemStack]]] = Sync[F].delay {
+    DB.readOnly { implicit session =>
+      sql"SELECT id, itemstack FROM gachadata WHERE itemstack NOT NULL"
+        .map { rs =>
+          val id = rs.string("id")
+          val path = Path(
+            NonEmptyVector(
+              Segment("gachadata"),
+              Vector(Segment(id))
+            )
+          )
+
+          serializeAndDeserialize.deserialize(
+            rs.string("itemstack")
+          )
+          .map(itemStack => DeserializedItemStacksWithPath(DeserializedItemStacks(Vector(itemStack)), path))
+          .merge
+        }
+        .toList
+        .apply()
+        .toVector
+    }
+  }
+
+  override def writeSerializedItemStacks(
+    deserializedItemStacksWithPaths: Vector[DeserializedItemStacksWithPath[ItemStack]]
+  ): F[Unit] = Sync[F].delay {
+    DB.localTx { implicit session =>
+      val batchParams = deserializedItemStacksWithPaths.map { deserializedItemStacksWithPath =>
+        Seq(
+          serializeAndDeserialize.serialize(
+            deserializedItemStacksWithPath.itemStacks.itemStacks.head
+          ),
+          deserializedItemStacksWithPath.path.segments.toVector.last.value,
+        )
+      }
+
+      sql"UPDATE gachadata SET itemstack = ? WHERE id = ?"
+        .batch(batchParams: _*)
+        .apply[List]()
+    }
+  }
+}
