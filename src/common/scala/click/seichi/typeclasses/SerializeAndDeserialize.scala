11527690f8f87447849e145ae8f6f9c74bfc13d0
rito528 <39003544+rito528@users.noreply.github.com>
2025-05-12 01:22:27 +0900

    feat: SerializeAndDeserialize を定義

---
+++ src/common/scala/click/seichi/typeclasses/SerializeAndDeserialize.scala
@@ -0,0 +1,21 @@
+package click.seichi.typeclasses
+
+/**
+ * シリアライズおよびデシリアライズが行える型の型クラス
+ */
+trait SerializeAndDeserialize[ParseError, T] {
+
+  /**
+   * [[T]] の値を文字列にシリアライズする。
+   */
+  def serialize(value: T): String
+
+  /**
+   * 文字列から [[T]] へのデシリアライズを試みる。
+   *
+   * [[T]] の値が構築できなかった場合は、理由を説明する
+   * [[ParseError]] が [[Left]] で返る。
+   */
+  def deserialize(serialized: String): Either[ParseError, T]
+
+}
