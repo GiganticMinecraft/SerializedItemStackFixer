958c08899542f4fe9c36ec45f626bd62de67d959
rito528 <39003544+rito528@users.noreply.github.com>
2025-05-15 02:43:30 +0900

    refactor: itemStackIntoWorld のリファクタリング

--- src/common/scala/click/seichi/ItemStackFixerAPI.scala
+++ src/common/scala/click/seichi/ItemStackFixerAPI.scala
@@ -2,7 +2,6 @@ package click.seichi
 
 import cats.effect.Sync
 import click.seichi.application.{ComputeLocationFromPaths, DeserializedItemStacksIntoChest, PutChest, WorldLifecycleManager}
-import click.seichi.domain.persistence.ItemStackPersistence
 import click.seichi.domain.WorldName
 import click.seichi.infra.{JdbcFourDimensionalPocketItemStackPersistence, JdbcGachaDataItemStackPersistence, JdbcSharedInventoryItemStackPersistence}
 import click.seichi.typeclasses.SerializeAndDeserialize
@@ -16,34 +15,22 @@ class ItemStackFixerAPI[F[_]: Sync, ItemStack] {
     putChest: PutChest[F],
     serializeAndDeserialize: SerializeAndDeserialize[Nothing, Vector[ItemStack]],
     worldLifecycleManager: WorldLifecycleManager[F]
-  ): F[Unit] = {
-    val fourDimensionalPocketItemStackPersistence: ItemStackPersistence[F, ItemStack] =
-      new JdbcFourDimensionalPocketItemStackPersistence[F, ItemStack]
-    val sharedInventoryItemStackPersistence: ItemStackPersistence[F, ItemStack] =
-      new JdbcSharedInventoryItemStackPersistence[F, ItemStack]
-    val gachaDataItemStackPersistence: ItemStackPersistence[F, ItemStack] =
-      new JdbcGachaDataItemStackPersistence[F, ItemStack]
-
-    val deserializedItemStacksWithPathF = Vector(
-      fourDimensionalPocketItemStackPersistence,
-      sharedInventoryItemStackPersistence,
-      gachaDataItemStackPersistence,
+  ): F[Unit] = for {
+    deserializedItemStacksWithPath <- Vector(
+      new JdbcFourDimensionalPocketItemStackPersistence[F, ItemStack],
+      new JdbcSharedInventoryItemStackPersistence[F, ItemStack],
+      new JdbcGachaDataItemStackPersistence[F, ItemStack],
     ).flatTraverse(_.readSerializedItemStacks)
+    worldName <- Sync[F].pure(WorldName("formigration"))
+    pathAndLocations <- Sync[F].pure(
+      ComputeLocationFromPaths.compute(deserializedItemStacksWithPath.map(_.path), worldName)
+    )
+    _ <- worldLifecycleManager.createWorld(worldName)
+    _ <- pathAndLocations.zip(deserializedItemStacksWithPath)
+      .traverse { case (pathAndLocation, deserializedItemStacksWithPath) =>
+        putChest.put(pathAndLocation.location) >> deserializedItemStacksIntoChest
+          .intoChest(deserializedItemStacksWithPath.itemStacks)
+      }
+  } yield ()
 
-    val worldName = WorldName("formigration")
-
-    for {
-      deserializedItemStacksWithPath <- deserializedItemStacksWithPathF
-      pathAndLocations <- Sync[F].pure(
-        ComputeLocationFromPaths.compute(deserializedItemStacksWithPath.map(_.path), worldName)
-      )
-      _ <- worldLifecycleManager.createWorld(worldName)
-      _ <- pathAndLocations.zip(deserializedItemStacksWithPath)
-        .traverse { case (pathAndLocation, deserializedItemStacksWithPath) =>
-          putChest.put(pathAndLocation.location) >> deserializedItemStacksIntoChest
-            .intoChest(deserializedItemStacksWithPath.itemStacks)
-        }
-    } yield ()
-
-  }
 }
