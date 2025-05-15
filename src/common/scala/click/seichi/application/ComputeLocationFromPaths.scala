2c2a8429e5a35de07c295f07db3ac06793d280d5
rito528 <39003544+rito528@users.noreply.github.com>
2025-05-16 02:14:21 +0900

    fix: assertion の条件ミスを修正

--- src/common/scala/click/seichi/application/ComputeLocationFromPaths.scala
+++ src/common/scala/click/seichi/application/ComputeLocationFromPaths.scala
@@ -11,7 +11,7 @@ object ComputeLocationFromPaths {
     val sideSize = minimumSideSize(paths.size)
 
     // NOTE: これを超えるのは対象となる Path の総数が 16,581,375 を超えたとき。
-    assert(sideSize > 255, "Total path size is too large!")
+    assert(sideSize < 255, s"Total path size(${paths.size}) is too large!")
 
     val savedLocations = for {
       x <- 0 to sideSize * 3 by 3
@@ -20,7 +20,7 @@ object ComputeLocationFromPaths {
     } yield LargeChestPuttedLocation(worldName, Coordinate(x, y, z))
 
     // NOTE: このアサーションが引っかかるならロジックが間違っている
-    assert(savedLocations.size <= paths.size)
+    assert(savedLocations.size >= paths.size)
 
     paths.zip(savedLocations).map { case (paths, location) =>
       PathAndLocation(paths, location)
