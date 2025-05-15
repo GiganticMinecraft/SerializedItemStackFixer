e3dbd4ea65614a6a789b220ad9a3bdc8a880d316
rito528 <39003544+rito528@users.noreply.github.com>
2025-05-16 01:20:44 +0900

    feat: Config 周りの定義を追加

---
+++ src/1-12/scala/click/seichi/Config.scala
@@ -0,0 +1,29 @@
+package click.seichi
+
+import org.bukkit.configuration.file.FileConfiguration
+import org.bukkit.plugin.java.JavaPlugin
+
+final class Config private (val config: FileConfiguration) {
+  def getHost: String = config.getString("host")
+  def getPort: Int = config.getInt("port")
+  def getUser: String = config.getString("user")
+  def getPassword: String = config.getString("password")
+
+  def getURL: String = {
+    val portComponent = {
+      val port = config.getString("port", "")
+      if (port.isEmpty) "" else ":" + port
+    }
+    val hostComponent = config.getString("host")
+
+    s"jdbc:mysql://$hostComponent$portComponent"
+  }
+}
+
+object Config {
+  def loadFrom(implicit plugin: JavaPlugin): Config = {
+    plugin.saveDefaultConfig()
+    plugin.reloadConfig()
+    new Config(plugin.getConfig)
+  }
+}
